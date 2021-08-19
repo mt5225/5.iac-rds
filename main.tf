16ab98f5f845d2e57464629c1bb2423ccbe0a78f
mt5225 <mt5225@gmail.com>
2021-08-18 17:22:57 -0700

    update

--- main.tf
+++ main.tf
@@ -19,9 +19,9 @@ module "vpc" {
   cidr = "10.99.0.0/18"
 
   azs              = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  public_subnets   = ["10.99.0.0/24", "10.99.1.0/24", "10.99.2.0/24"]
-  private_subnets  = ["10.99.3.0/24", "10.99.4.0/24", "10.99.5.0/24"]
-  database_subnets = ["10.99.7.0/24", "10.99.8.0/24", "10.99.9.0/24"]
+  private_subnets  = cidrsubnets("10.99.1.0/20", 4, 4, 4)
+  public_subnets   = cidrsubnets("10.99.51.0/20", 4, 4, 4)
+  database_subnets = cidrsubnets("10.99.101.0/20", 4, 4, 4)
 
   create_database_subnet_group = true
 
